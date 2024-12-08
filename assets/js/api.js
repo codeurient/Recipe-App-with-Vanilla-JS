5f8ab641670f4b90031d48d43d911a2b522d79ff
codeurient <codeurient@gmail.com>
2024-12-08 09:54:19 +0400

    API

--- assets/js/api.js
+++ assets/js/api.js
@@ -5,3 +5,33 @@
  */
 
 "use strict";
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+const /** {String} */ API_ID = "a6e5a5b3";
+const /** {String} */ API_KEY = "d6a6e5a5b3a6e5a5b3a6e5a5b3a6e5a5";
+const /** {String} */ TYPE = "public";
+
+
+/**
+ * @param {Array} queries Query array
+ * @param {Function} successCallback Success callback function
+*/
+
+export const fetchData = async function(queries, successCallback) {
+    const /** {String} */ query = queries?.join("&")
+        .replace(/,/g, "=")
+        .replace(/ /g, "%20")
+        .replace(/\+/g, "%2B");
+
+    const /** {String} */ url = `${ACCESS_POINT}?app_id=${API_ID}&app_key=${API_KEY}&type=${TYPE}${query ? `&${query}` : ""}`;
+
+
+    const /** {Object} */ response = await fetch(url);
+
+    if(response.ok) {
+        const data = await response.json();
+        successCallback(data);
+    }
+
+
+}
